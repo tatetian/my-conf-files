4f1d77c8f8a3c451a8d171b3ea7de4cd4d7fb3b7
Hongliang Tian <tatetian@gmail.com>
2013-12-07 15:27:01 +0800

    Add git configuration

--- misc/install.sh
+++ misc/install.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+# tmux
 cp tmux.conf ~/.tmux.conf
+# git
+git config --global user.name   "Tate, Hongliang Tian"
+git config --global user.email  "tatetian@gmail.com"
+git config --global core.editor "vim"
+git config --global credential.helper cache
