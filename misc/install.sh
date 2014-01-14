37cdcc2e98d7ce401a341f6f800b12601e6613aa
Tate, Hongliang Tian <tatetian@gmail.com>
2014-01-14 16:33:45 +0800

    Add inpurc to make bashrc works in vim mode

--- misc/install.sh
+++ misc/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # tmux
 cp tmux.conf ~/.tmux.conf
+cp inputrc   ~/.inputrc
 # git
 git config --global user.name   "Tate, Hongliang Tian"
 git config --global user.email  "tatetian@gmail.com"
