8a82cb4e0a9dfa584c07efba0c2e5d03f707ef78
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-05 21:00:42 +0800

    Add molokai theme for vim-airline

--- vim/install.sh
+++ vim/install.sh
@@ -3,3 +3,6 @@ cp vimrc.local ~/.vimrc.local
 cp vimrc.before ~/.vimrc.before
 cp vimrc.bundles.local ~/.vimrc.bundles.local
 cp -r plugin ~/.vim/
+
+# install modified molokai theme for vim-airline
+cp molokai.vim ~/.vim/bundle/vim-airline/autoload/airline/themes/molokai.vim
