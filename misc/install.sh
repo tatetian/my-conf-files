1a5a27e227d53ae674ea53c3df7949e63b409d4a
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-05 12:48:12 +0800

    Fix tmux clipboard problem in MacOS X

--- misc/install.sh
+++ misc/install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 # tmux
 cp tmux.conf ~/.tmux.conf
+# Fix tmux to access MaxOSX clipboard
+if [[ "$(uname)" = "Darwin" ]]; then
+    echo "\nset-option -g default-command \"reattach-to-user-namespace -l bash\"" >> ~/.tmux.conf
+fi
+#tmux source-file ~/.tmux.conf
+
 cp inputrc   ~/.inputrc
+
 # git
 git config --global user.name   "Tate, Hongliang Tian"
 git config --global user.email  "tatetian@gmail.com"
