b4775b5ff206b8f9b34eb8f8e82890182a8c3b06
Tate, Hongliang Tian <tatetian@gmail.com>
2017-08-17 17:30:05 +0800

    Fix a bug that breaks powerline-font of airline vim plugin

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp vimrc.local ~/.vimrc.local
-cp vimrc.before ~/.vimrc.before
+cp vimrc.before.local ~/.vimrc.before.local
 cp vimrc.bundles.local ~/.vimrc.bundles.local
 cp -r plugin ~/.vim/
 
